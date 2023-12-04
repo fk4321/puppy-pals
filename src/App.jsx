f20e1e5cd8459a842be295a3a25ca27697507104
fk4321 <139650641+fk4321@users.noreply.github.com>
2023-12-04 17:25:38 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-    console.log(puppies)
-  return (
+    // console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  // console.log(featPupId)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
+   return (
     <>
       <div className="App">
         <h1>Puppy Pal - Workshop</h1>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p key={puppy.id} onClick={() => {
+            setFeatPupId(puppy.id)
+            // const featuredPup = puppies.find((pup) => pup.id === featPupId)
+            // console.log(featuredPup)
+          }}>{puppy.name}</p>
         })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+
+        )
+        }
       </div>
     </>
   )
