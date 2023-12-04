272b9be00b72c1305b954fa8afb2758db988b7ea
fk4321 <139650641+fk4321@users.noreply.github.com>
2023-12-04 15:31:45 -0500

    updated function app to render puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList)
+    console.log(puppies)
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
+      <div className="App">
+        <h1>Puppy Pal - Workshop</h1>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })}
       </div>
     </>
   )
